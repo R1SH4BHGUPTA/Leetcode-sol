4b981245851968bff044f8f87d31d1d07483a426
R1SH4BHGUPTA <r1sh4bhgupta@gmail.com>
2026-08-10 18:20:38 +0530

    Time: 14 ms (91.38%) | Memory: 41.9 MB (99.48%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        // dp[0] = false
+        // No stones -> player cannot move -> loses
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                // If removing j*j stones makes opponent lose
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
