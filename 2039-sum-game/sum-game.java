126cccc53cbd600f8321aaa58fe3d872ed08db1c
R1SH4BHGUPTA <r1sh4bhgupta@gmail.com>
2026-08-23 18:54:03 +0530

    Time: 6 ms (97.08%) | Memory: 47.3 MB (10.95%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        // Alice gets the last move.
+        // She can always break equality.
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // If equal number of '?' on both sides,
+        // Bob can mirror Alice's moves.
+        if (leftQ == rightQ) {
+            return leftSum != rightSum;
+        }
+
+        /*
+         * Bob wins only if the existing difference can be
+         * exactly compensated by the unmatched '?'s.
+         *
+         * Each pair contributes 9 to the difference.
+         */
+        return leftSum - rightSum
+                != 9 * (rightQ - leftQ) / 2;
+    }
+}
